747364fc434390cd4002367d65c15bb7d6aa4cba
Julietobiyor <obiyorjuliet@gmail.com>
2023-11-22 01:14:13 -0800

    task 6 updated

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,72 +1,57 @@
 #!/usr/bin/python3
-"""
-square class
-"""
+'''
+A module for working with squares.
+'''
 
 
 class Square:
-
+    '''
+    This represents a 2D Polygon
+    with 4 equal and perpendicular sides.
+    '''
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
-    """
-    creates a square object
-    """
+
     @property
     def size(self):
         return self.__size
-        """
-        gets size
-        """
+
     @property
     def position(self):
         return self.__position
-        """
-        gets position
-        """
-    @position.setter
-    def position(self, value):
-        if(type(value) is not tuple or len(value) is not 2 or
-           type(value[0]) is not int or
-           type(value[1]) is not int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if(value[0] < 0 or value[1] < 0):
-            raise ValueError("position must be a tuple of 2 positive integers")
-        self.__position = value
 
-        """
-        sets position
-        position has to be a tuple of positive integers
-        Raise:
-            ValueError
-            TypeError
-        """
     @size.setter
     def size(self, value):
-        if(type(value) is not int):
-            raise TypeError("size must be an integer")
-        elif(value < 0):
-            raise ValueError("size must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
         else:
-            self.__size = value
-        """
-        sets size
-        size has to be an integer and positive
-        """
+            if value < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = value
+
+    @position.setter
+    def position(self, value):
+        is_invalid_value = True
+        error_msg = 'position must be a tuple of 2 positive integers'
+        if isinstance(value, tuple):
+            if len(value) == 2:
+                if isinstance(value[0], int) and isinstance(value[1], int):
+                    if value[0] >= 0 and value[1] >= 0:
+                        is_invalid_value = False
+        if is_invalid_value:
+            raise TypeError(error_msg)
+        else:
+            self.__position = value
 
     def area(self):
-        return(self.__size**2)
-        """
-        returns the area of the size of the square
-        """
+        return self.size ** 2
+
     def my_print(self):
-        if(self.size == 0):
-            print()
-            return
-        for x in range(self.position[1]):
-            print()
-        for x in range(self.size):
-            print("{}{}".format(" " * self.position[0], "#" * self.size))
-        """
-        prints a square of hashtags based on position and size
-        """
+        if self.size == 0:
+            print('')
+        else:
+            print('{}'.format('\n' * self.position[1]), end='')
+            for i in range(self.size):
+                print('{}{}'.format(' ' * self.position[0], '#' * self.size))
