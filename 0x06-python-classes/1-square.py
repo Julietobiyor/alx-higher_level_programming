30c8b89a1d3652b8ab43352c779524dd0140e076
Julietobiyor <obiyorjuliet@gmail.com>
2023-11-21 20:54:48 -0800

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+"""Square module."""
+
+
 class Square:
-    """
-    creates a square class object
-    """
+    """Defines a square."""
+
     def __init__(self, size):
+        """Constructor.
+        Args:
+            size: length of side of the square.
+        """
         self.__size = size
-    """
-    initializes a square object with parameters
-    args:
-        __size(int): size of the square
-    """
