717d4f27bc8580ec688e0ab817b0cc5239dc4b0b
Julietobiyor <obiyorjuliet@gmail.com>
2023-11-22 01:18:48 -0800

    task 100 updated

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-class Node:
+'''
+A module for working with singly linked lists.
+'''
 
+
+class Node:
+    '''
+    Represents a node in a singly linked list.
+    '''
     def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
@@ -9,36 +16,52 @@ class Node:
     def data(self):
         return self.__data
 
-    @data.setter
-    def data(self, value):
-        if(not isinstance(value, int)):
-            raise TypeError("data must be an integer")
-        self.__data = value
-
     @property
     def next_node(self):
         return self.__next_node
 
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError('data must be an integer')
+        else:
+            self.__data = value
+
     @next_node.setter
     def next_node(self, value):
-        if(not isinstance(value, Node) and value is not None):
-            raise TypeError("next_node must be a Node object")
-        self.__next_node = value
+        if value is None or isinstance(value, Node):
+            self.__next_node = value
+        else:
+            raise TypeError('next_node must be a Node object')
 
 
 class SinglyLinkedList:
-    __head = None
-
+    '''
+    Represents a singly linked list.
+    '''
     def __init__(self):
-        pass
-
-    def __str__(self):
-        newList = []
-        while(self.__head):
-            newList.append(self.__head.data)
-            self.__head = self.__head.next_node
-        return("\n".join([str(x) for x in sorted(newList)]))
+        self.__head = None
 
     def sorted_insert(self, value):
-        newNode = Node(value, self.__head)
-        self.__head = newNode
+        if not isinstance(value, int) or value is None:
+            raise TypeError('value must be an integer')
+        else:
+            if self.__head is None or self.__head.data >= value:
+                new_node = Node(value, self.__head)
+                self.__head = new_node
+            else:
+                node_ptr = self.__head
+                prev_ptr = None
+                while node_ptr is not None and value > node_ptr.data:
+                    prev_ptr = node_ptr
+                    node_ptr = node_ptr.next_node
+                new_node = Node(value, node_ptr)
+                prev_ptr.next_node = new_node
+
+    def __str__(self):
+        node_ptr = self.__head
+        res = []
+        while node_ptr is not None:
+            res.append(str(node_ptr.data))
+            node_ptr = node_ptr.next_node
+        return '' if len(res) == 0 else '\n'.join(res)
