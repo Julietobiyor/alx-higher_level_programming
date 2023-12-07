e4d2299ebd7b85ff0de5e30e140a7bf73dc0dcb7
Julietobiyor <obiyorjuliet@gmail.com>
2023-12-06 21:05:27 -0800

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
-"""function"""
-from json import JSONDecoder
+"""A script that stores arguments to a JSON file."""
+import sys
+import os
+from importlib import import_module as using
 
 
-def load_from_json_file(filename):
-    """function that creates an Object from a “JSON file”"""
-    lines = []
-    with open(filename, encoding='utf-8') as file:
-        for line in file.readlines():
-            lines.append(line)
-    return JSONDecoder().decode(''.join(lines))
+save_to_json_file, load_from_json_file = (
+    using('5-save_to_json_file').save_to_json_file,
+    using('6-load_from_json_file').load_from_json_file
+)
+args_list = []
+file_name = 'add_item.json'
+
+
+def makeScript():
+    if not os.path.exists(file_name):
+        with open(file_name, mode='w', encoding='utf-8') as file:
+            file.write('[]')
+    json_list = load_from_json_file(file_name)
+    if (type(json_list) is list) and all(type(s) is str for s in json_list):
+        args_list.extend(json_list)
+    args_list.extend(sys.argv[1:])
+    save_to_json_file(args_list, file_name)
+
+
+if __name__ == '__main__':
+    makeScript()
