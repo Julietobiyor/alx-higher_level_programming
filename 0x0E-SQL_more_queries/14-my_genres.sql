dc1a63013aa890801bc7e5d3d63d8405c63c5e27
Juliet obiyor <obiyorjuliet@gmail.com>
2024-01-17 16:06:48 -0800

    updates

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,15 @@
--- genre id by show
-SELECT tgg.name FROM tv_genres AS tgg
-JOIN tv_show_genres AS tgs
-ON tgs.genre_id = tgg.id
-JOIN tv_shows AS ts
-ON ts.id = tgs.show_id
-WHERE ts.title='Dexter'
-GROUP BY tgg.name ASC;
+-- lists all genres of the show Dexter.
+-- The tv_shows table contains only one record where title = Dexter (but the id can be different)
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name;
